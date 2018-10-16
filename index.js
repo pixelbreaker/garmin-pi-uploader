fa479a9553b39ba4eba4b6647d92196357dc8f45
Gabes Mak <gabriel@pixelbreaker.com>
2018-10-16 21:38:42 +0100

    import totemize

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Dropbox = require('dropbox').Dropbox
 const fetch = require('node-fetch')
 const dbx = new Dropbox({ accessToken: config.env.DROPBOX_ACCESS_TOKEN, fetch: fetch })
 const strava = require('strava-v3')
-// const totemize = require('totemize')
+const totemize = require('totemize')
 
 const totemConfig = require('./totem.json')
 
